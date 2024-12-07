a2cacceb8f4761b21113dfba48a29e12dfefa65d
Vare <dm.va@hotmail.com>
2024-12-07 05:23:06 -0300

    second commit

---
+++ frontend/src/components/MovingBar.tsx
@@ -0,0 +1,28 @@
+import 'vite-express-ecom-starter/frontend/src/styles/MovingBlackBar.css'
+
+const MovingBar: React.FC = () => {
+  return (
+      <div className="relative w-full h-9 overflow-hidden bg-black">
+        <div className="absolute top-0 left-0 w-full h-full bg-black text-xs text-white flex items-center justify-center animate-slide-left-right">
+          <div className="flex-shrink-0 whitespace-nowrap">
+            <p className="text-center">
+              25% OFF EFECTIVO / TRANSFERENCIA &nbsp;&nbsp;&nbsp;&nbsp; 
+              6 CUOTAS SIN INTERES + ENVIOS A TODA CABA &nbsp;&nbsp;&nbsp;&nbsp; 
+              WINTER SALE VUELA TODO! &nbsp;&nbsp;&nbsp;&nbsp;
+              25% OFF EFECTIVO / TRANSFERENCIA &nbsp;&nbsp;&nbsp;&nbsp; 
+              6 CUOTAS SIN INTERES + ENVIOS A TODA CABA &nbsp;&nbsp;&nbsp;&nbsp; 
+              WINTER SALE VUELA TODO! &nbsp;&nbsp;&nbsp;&nbsp;
+              25% OFF EFECTIVO / TRANSFERENCIA &nbsp;&nbsp;&nbsp;&nbsp; 
+              6 CUOTAS SIN INTERES + ENVIOS A TODA CABA &nbsp;&nbsp;&nbsp;&nbsp; 
+              WINTER SALE VUELA TODO! &nbsp;&nbsp;&nbsp;&nbsp;
+              25% OFF EFECTIVO / TRANSFERENCIA &nbsp;&nbsp;&nbsp;&nbsp; 
+              6 CUOTAS SIN INTERES + ENVIOS A TODA CABA &nbsp;&nbsp;&nbsp;&nbsp; 
+              WINTER SALE VUELA TODO! &nbsp;&nbsp;&nbsp;&nbsp;
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  };
+  
+  export default MovingBar;
